cccd5ad94c4a5830b78b78d03c1c61034bef4e30
Tassa Tenhouse <kmtenhouse@gmail.com>
2018-11-01 23:40:53 -0700

    Fixed extraneous console log in supervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -20,7 +20,6 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
  //show the manager functions!
     supervisorMenu();
 });
